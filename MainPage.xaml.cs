0686702d6140fdc95c6bf6749d584a9fd0af7cde
Monty Sinha <monty.sinha2013@live.co.uk>
2020-03-12 15:42:57 +0000

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobileapp3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Handle_Clicked(object sender,System.EventArgs e)
+        {
+           string username= nameEntry.Text;
+            string greeting = $"Hello{username}";
+            GreetingLabel.Text = greeting;
+        }
+    }
+}
